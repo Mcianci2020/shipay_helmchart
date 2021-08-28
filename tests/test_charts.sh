518f04d41a972c2811a54389cf6ce8c84bfd6967
Marcos Cianci <marcos.cianci@gmail.com>
2021-08-28 14:25:22 -0300

    Create script test helm charts

---
+++ tests/test_charts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+declare -a CHARTS_ARRAY=("fastAPI" "faust")
+
+for i in "${CHARTS_ARRAY[@]}"
+do
+    CHART_NAME="$i"
+    echo -e "\n----------------------------"
+    echo "Testing chart: $CHART_NAME"
+    echo -e "----------------------------\n"
+
+    # Replace false with true to enable all chart features before test
+    sed "s/false/true/" charts/$CHART_NAME/values.yaml > charts/$CHART_NAME/test-values.yaml
+    helm template new_app_name --debug --v 2 charts/$CHART_NAME --values=charts/$CHART_NAME/test-values.yaml
+    rm charts/$CHART_NAME/test-values.yaml
+
+done
