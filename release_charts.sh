942e667b742daa2e2969461f21032603b639b571
Marcos Cianci <marcos.cianci@gmail.com>
2021-08-28 21:20:30 -0300

    Create script release helm chart

---
+++ release_charts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PROJECT_DIR=$(pwd)
+
+declare -a CHARTS_ARRAY=("fastAPI" "faust" )
+
+for i in "${CHARTS_ARRAY[@]}"
+do
+    CHART_NAME="$i"
+    echo "Building chart: $CHART_NAME"
+    mkdir -p releases/$CHART_NAME
+
+    # packaging chart
+    helm package charts/$CHART_NAME/
+    # Move if not exists
+    cp -n $CHART_NAME*.tgz releases/$CHART_NAME
+
+    # Remove all .tgz generated
+    rm $PROJECT_DIR/*.tgz
+
+    cd $PROJECT_DIR && helm repo index releases
+done
+
+for i in "${CHARTS_ARRAY[@]}"
+do
+    CHART_NAME="$i"
+    echo "Releasing chart: $CHART_NAME"
+    sed -i "s/- $CHART_NAME/- releases\/$CHART_NAME/" releases/index.yaml
+done
+
+# Move index.yaml to root
+mv releases/index.yaml $PROJECT_DIR
